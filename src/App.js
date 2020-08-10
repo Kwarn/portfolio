e7a1cf8dc6b8c5f4f5195ed4c7ffd6b1a8e07c45
Kwarn <karlwarner.dev@gmail.com>
2020-08-10 11:38:56 +0100

    refactoring App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = props => {
       <Route
         path="/education"
         render={() => (
-          <Suspense fallback={<Home />}>
+          <Suspense fallback={<div>Something Went Wrong...</div>}>
             <Education />
           </Suspense>
         )}
