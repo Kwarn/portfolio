b92f206a98438e71e899512e395af22a07c16cba
Kwarn <karlwarner.dev@gmail.com>
2020-08-10 11:38:04 +0100

    Lazyloading & refactoring

--- src/App.js
+++ src/App.js
@@ -3,28 +3,30 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 
 import Home from './containers/Home/Home'
 import Layout from './Hoc/Layout/Layout'
-import Contact from './containers/Contact/Contact'
 import classes from './App.module.css'
 
-const ProjectsOverview = React.lazy(() => import('./containers/Projects/ProjectsOverview/ProjectsOverview'))
+const ProjectsOverview = React.lazy(() =>
+  import('./containers/Projects/ProjectsOverview/ProjectsOverview')
+)
 const Education = React.lazy(() => import('./components/Education/Education'))
+const Contact = React.lazy(() => import('./containers/Contact/Contact'))
 
 const App = props => {
   let routes = (
     <Switch>
       <Route
-        path="/education"
-        render={() => (
-          <Suspense fallback={<Home />}>
-            <Education />
-          </Suspense>
-        )}
-      />
-      <Route
         path="/projects"
         render={() => (
           <Suspense fallback={<div>Something Went Wrong...</div>}>
             <ProjectsOverview />
+          </Suspense>
+        )}
+      />
+      <Route
+        path="/education"
+        render={() => (
+          <Suspense fallback={<Home />}>
+            <Education />
           </Suspense>
         )}
       />
@@ -41,7 +43,11 @@ const App = props => {
     </Switch>
   )
 
-  return <div className={classes.App}><Layout>{routes}</Layout></div>
+  return (
+    <div className={classes.App}>
+      <Layout>{routes}</Layout>
+    </div>
+  )
 }
 
 export default App
