51d1a7ffaadd078011f8afb884e05ef32623e396
Kwarn <karlwarner.dev@gmail.com>
2020-08-13 18:48:38 +0100

    opacity for projects 90%

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import classes from './AboutMe.module.css'
 
-const AboutMe = (props) => {
+const AboutMe = props => {
   return (
     <div className={classes.AboutMe}>
       <h1>About Me</h1>
+      <p>
+        
+      </p>
     </div>
   )
 }
 
 export default AboutMe
-
