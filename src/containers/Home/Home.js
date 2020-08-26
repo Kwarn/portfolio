8d59ee2b3f4083c46b1fa2cb4ea15c3f35d0fc85
Kwarn <karlwarner.dev@gmail.com>
2020-08-26 15:49:56 +0100

    WelcomeElements made into their own component, project css to stop buggy FadeInSection

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,48 +5,13 @@ import Skills from '../../components/Skills/Skills'
 import Projects from '../Projects/Projects'
 import Courses from '../Courses/Courses'
 import FadeInSection from '../FadeInSection/FadeInSection'
+import WelcomeElements from '../../components/WelcomeElements/WelcomeElements'
 
 const Home = props => {
   return (
     <>
       <div className={classes.Home}>
-        <div className={classes.WelcomeElements}>
-          <div className={classes.Title}>
-            <img
-              className={classes.ProfileImage}
-              src={imageAssets.profileImage}
-              alt="profileImage"
-            />
-            <div className={classes.TitleElementsGroup}>
-              <h1>Karl Warner</h1>
-              <p>{`</> Software Developer`}</p>
-              <div className={classes.LinkIcons}>
-                <a
-                  href="https://github.com/Kwarn/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={imageAssets.gitHub} alt="gitHubIcon"></img>
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/karl-warner-9147661b5/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={imageAssets.linkedIn} alt="linkedInIcon"></img>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className={classes.Divider}></div>
-          <p className={classes.AboutMe}>
-            As a technical and mechanical tinkerer from a young age, I've always
-            been drawn to understanding how things work and finding logical
-            solutions to problems. My curiosity has led me to understand a broad
-            range of different technologies and I'd like to continue my journey
-            inside a skilled team that I can learn from and grow with.
-          </p>
-        </div>
+        <WelcomeElements />
         <FadeInSection fadeDirection="top">
           <img
             className={classes.OpenDrawIcon}
