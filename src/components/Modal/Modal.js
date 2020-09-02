c295fd1165fd7e33db9dde79bd570cc7f4927918
Kwarn <karlwarner.dev@gmail.com>
2020-09-02 17:33:30 +0100

    modal close icon added

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './Modal.module.css'
 import Backdrop from '../Backdrop/Backdrop'
+import imageAssets from '../../assets/assets'
 
 const modal = ({ show, hide, children }) => {
   return (
@@ -13,8 +14,8 @@ const modal = ({ show, hide, children }) => {
           opacity: show ? '1' : '0',
         }}
       >
+        <img onClick={hide} className={classes.CloseIcon} src={imageAssets.closeDraw} alt="close modal"></img>
         <div className={classes.Content}>{children}</div>
-        <button onClick={hide}>Close</button>
       </div>
     </>
   )
