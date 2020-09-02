0d7795854ea70629a65babf25fefd1dbaa99d05d
Kwarn <karlwarner.dev@gmail.com>
2020-09-02 11:22:37 +0100

    projects content updates

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -18,16 +18,10 @@ const Projects = props => {
         'https://www.udemy.com/course/react-the-complete-guide-incl-redux/',
       description: `This app allows users to create and order their own customized burger.
 
-       Functionalities include form validation, authentication, database management,
-        automatic login and preservation of the users session.`,
+       Functionalities include form validation, authentication, database management, automatic login and preservation of the users session.`,
       whatILearned: `This project was instrumental in developing an effective & efficient
-       approach to learning. 
-       Prior to this project a "well it works, so move on" mentality was costing me
-       alot of technical debt, delays in progress and resulted in demotivation.
-       I learned the importance of taking the time to really understand and dive deeper
-       into difficult concepts instead of shying away when my head began to hurt. 
-       I found taking notes and explaining outloud the problem and possible solutions
-       greatly helped reinforce the lessons I was learning.`,
+      approach to learning new concepts. 
+      Prior to this project a "well it works, move on" approach was costing me a lot of technical debt, delays in progress and resulted in demotivation. I learned the importance of taking the time to really understand and dive deeper into difficult concepts instead of shying away when my head began to hurt. I found taking notes and explaining out-loud the problem and possible solutions greatly helped reinforce the lessons I was learning.`,
       technicalPractices: `Multiple approaches to Handling State, Authentication tokens 
         and localStorage, Asynchronous actions, Class-based components 
         and lifecycle hooks, Functional components and react hooks, 
@@ -44,8 +38,7 @@ const Projects = props => {
 
        This was my first practice app diving into the world of javascript and React, the app was built mostly on guess work and google searches.`,
       whatILearned: `This project taught me the importance of how to effectively ask questions and find answers without burdening the people who were offering help with something I could have found out myself.
-      
-      An early build of this project was having major preformance issues caused by large image file sizes and complicated logic. While in persuit of a solution to the complicated logic, I was introduced to Big O notation which helped me to understand why and how to refactor the code.`,
+      An early build of this project was having major performance issues caused by large image file sizes and complicated logic. While in pursuit of a solution to the complicated logic, I was introduced to Big O notation which helped me to understand why and how to refactor the code.`,
       technicalPractices: 'Fundamentals of state and data management',
     },
     fitness: {
@@ -56,7 +49,10 @@ const Projects = props => {
 
       gitHubLink: 'https://github.com/Kwarn/fitness',
       description: `A light-weight MyFitnessPal clone, this app allows users to save their meal history and add new meals with their ingredients and calorie information to a SQlite database.`,
-      whatILearned: ``,
+      whatILearned: `I learned the importance of not reinventing the wheel (Ignoring the irony of this being a copy of another app) when it came to data management.
+
+      My initial approach to storing data was using the file system to read/write to a .txt file. I found this approach to be extremely cumbersome. In my search for a better solution I took the time to weight-up the different options and settled on using SQLite database. 
+      I learned the importance of looking for a cleaner, established solution before implementing my own.`,
       technicalPractices: 'Database queries',
     },
     fizzyLookup: {
@@ -66,11 +62,10 @@ const Projects = props => {
       gitHubLink: 'placeholder',
       description: `Desktop App which allows easier searching of excel data.
 
-        Built on request for a friend, this project allowed me demonstate and practice a real world use-case of what I had learned with Python, helping them to work more efficiently on what was previously a time consuming and teedious task.
-
-        The app uses pandas (data analysis libary) to convert an excel spread-sheet to columns and rows displayed by tkinter (GUI libary).
-        
-        The excel spread-sheet contains property data such as rental price & number of bedrooms. The user can accurately search the data using a combination of 7 dropdown menus.`,
+      Built on request for a friend, this project allowed me demonstrate and practice a real world use-case of what I had learned with Python, helping them to work more efficiently on what was previously a time consuming and tedious task.
+      
+      The app uses pandas (data analysis library) to convert an excel spread-sheet to columns and rows displayed by tkinter (GUI library).
+      The excel spread-sheet contains property data such as rental price & number of bedrooms. The user can accurately search the data using a combination of 7 drop-down menus.`,
       whatILearned: `The importance of accurately communicating a description 
         of the clients needs so that unnecessary or miscommunicated functionality
         didn't waste development time.`,
