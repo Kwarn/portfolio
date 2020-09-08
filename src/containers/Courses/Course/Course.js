3e1d9a9bc1fc74557a307c8f4c370e321cc025bc
Kwarn <karlwarner.dev@gmail.com>
2020-09-08 12:36:00 +0100

    clarified variable names, course no longer controls margin left/right

--- src/containers/Courses/Course/Course.js
+++ src/containers/Courses/Course/Course.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import imageAssets from '../../../assets/assets'
-import Draw from '../../Draw/Draw'
+import DrawToggle from '../../DrawToggle/DrawControl'
 import classes from './Course.module.css'
 
 const Course = ({ course, showModal, isHighlighted, elementRef }) => {
@@ -8,13 +8,7 @@ const Course = ({ course, showModal, isHighlighted, elementRef }) => {
 
   const certificate = <img src={cert} alt={`${title} certificate`} />
 
-  const drawContentProps = {
-    desc: desc,
-    isAlwaysShown:
-      title === 'Javascript - The Complete Guide 2020 (Beginner + Advanced)',
-  }
-
-  const certificateImage = cert ? (
+  const previewCertificate = cert ? (
     <img
       onClick={() => showModal(certificate)}
       src={cert}
@@ -30,14 +24,14 @@ const Course = ({ course, showModal, isHighlighted, elementRef }) => {
         }`}
       >
         <h1 className={classes.TitleContainer}>
-          {title}{' '}
+          {title}
           <a href={courseLink} target="_blank" rel="noopener noreferrer">
             <img src={imageAssets.magnifyingGlass} alt="showCourse" />
           </a>
         </h1>
-        <div className={classes.CertificateContainer}>{certificateImage}</div>
+        <div className={classes.CertificateContainer}>{previewCertificate}</div>
       </div>
-      <Draw contentType="course" drawContentProps={drawContentProps} />
+      <DrawToggle contentType="course" drawContent={desc} />
     </div>
   )
 }
