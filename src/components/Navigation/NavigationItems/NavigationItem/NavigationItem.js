b907628e81d7fab9127911731cc346a2c360893d
Kwarn <karlwarner.dev@gmail.com>
2020-09-23 13:44:35 +0100

    margin adjusted to account for new download link

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import classes from './NavigationItem.module.css'
 
-const navItem = ({ link, children }) => (
+const navItem = ({ link, children}) => (
   <li className={classes.NavigationItem}>
     <NavLink exact activeClassName={classes.active} to={link}>
       {children}
