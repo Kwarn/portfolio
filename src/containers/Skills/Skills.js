f34338a5ed33b04b66c63c567a6c8318c32e2f0c
Kwarn <karlwarner.dev@gmail.com>
2020-09-23 14:02:03 +0100

    skills state converted to array to allow mapping of child components, delay render duration added to skill objects

--- src/containers/Skills/Skills.js
+++ src/containers/Skills/Skills.js
@@ -1,48 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Skill from './Skill/Skill'
 import imageAssets from '../../assets/assets'
 import classes from './Skills.module.css'
 
 const Skills = () => {
-  const [skills] = useState({
-    javascript: {
+  const [skills] = useState([
+    {
       title: 'Javascript',
       content: `ES6+, React, React-Router, Redux, Redux-Saga, Thunk, Axios, Enzyme, NodeJs, NextJs.`,
       image: imageAssets.javascript,
       fadeInAndAlignDirection: 'left',
+      delayRenderDuration: 0,
     },
-    python: {
+    {
       title: 'Python',
       content: `TkInter, Django, SQLite, PyGame`,
       image: imageAssets.python,
       fadeInAndAlignDirection: 'right',
+      delayRenderDuration: 400,
     },
-    general: {
+    {
       title: 'General',
       content: `UI/UX design, SQL, HTML, CSS, GitHub, Unix Commands, Dev-Tools, Firebase, Webpack, Photoshop, Gimp, Adobe Premiere Pro..`,
       image: imageAssets.uiUx,
       fadeInAndAlignDirection: 'left',
+      delayRenderDuration: 800,
     },
-    devEnv: {
+    {
       title: 'Dev Environment',
       content: `Ubuntu 20.04, Windows 10 with GitBash, VSCode, Pycharm, Firefox, Chrome.`,
       image: imageAssets.visualStudioCode,
       fadeInAndAlignDirection: 'right',
+      delayRenderDuration: 1200,
     },
-  })
+  ])
 
-  const skillSections = []
-  let delay = 0
-  for (let skill in skills) {
-    skillSections.push(
-      <Skill
-        {...skills[skill]}
-        delayRenderDuration={delay}
-      />
-    )
-    delay += 500
-  }
-
+  const skillSections = skills.map(skill => (
+    <Skill
+      key={skill.title}
+      {...skill}
+      delayRenderDuration={skill.delayRenderDuration}
+    />
+  ))
   return <div className={classes.Skills}>{skillSections}</div>
 }
 
