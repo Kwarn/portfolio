abb8d462247cd3e4f20e2481452f2866f0be9ad0
Kwarn <karlwarner.dev@gmail.com>
2020-09-23 13:45:28 +0100

    added download CV button & file-saver

--- src/components/Navigation/NavigationItems/NavigationItemsRight/NavigationItemsRight.js
+++ src/components/Navigation/NavigationItems/NavigationItemsRight/NavigationItemsRight.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import FileSaver from 'file-saver'
 import NavigationItem from '../NavigationItem/NavigationItem'
 import classes from './NavigationItemsRight.module.css'
+
+const saveFile = () =>
+  FileSaver.saveAs(process.env.PUBLIC_URL + '/resource/test.jpg', 'test.jpg')
 
 const navigationItemsRight = props => {
   return (
     <ul className={classes.NavigationItems}>
+      <div className={classes.DownloadCV} onClick={saveFile}>
+        Download CV
+      </div>
       <NavigationItem link="/contact">Contact</NavigationItem>
     </ul>
   )
