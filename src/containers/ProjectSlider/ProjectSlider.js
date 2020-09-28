3024bbb9b9de914124eb65ebb2ff73895c8cb0fe
Kwarn <karlwarner.dev@gmail.com>
2020-09-28 18:28:58 +0100

    project descriptions updated

--- src/containers/ProjectSlider/ProjectSlider.js
+++ src/containers/ProjectSlider/ProjectSlider.js
@@ -11,12 +11,13 @@ const ProjectSlider = ({ showModal }) => {
       image: imageAssets.portfolio,
       previewTechStack: 'Javascript, React, Firebase, CSS',
       gitHubLink: 'https://github.com/Kwarn/portfolio',
-      description: `The portfolio has seen many diffrent design ideas and has served as a live enviroment to recieve feedback on and consider many different perspectives.`,
-      whatILearned: `Early on in the project I had created the draw component, being proud of my work I committed for too long to create the UI/UX around it.
-      I realised that the user exeperience would suffer when being forced to click most elements to view information which should be always clearly displayed to the user.
-      
-      I learned the importance of giving up on a bad idea as early as possible so not too waste developement time.`,
-      technicalPractices: 'Css',
+      description: `Serving as a live environment to create and tinker with components, the portfolio has been an invaluable resource allowing me to practise a variety of design approaches, receive feedback and consider different perspectives.`,
+      whatILearned: `I learned the importance of remaining objective to prevent the over implementation of a feature I was proud of having detrimental effects to user’s experience and their time constraints.
+
+      Early on in the project I used the creation of the ‘Draw/More Info’ component (bottom right) to challenge my theoretical implementation and restrain from referring to google. Success served to reinforce my technical understanding and boost my confidence.
+      However, I realised the user experience would suffer when being forced to click most elements to view high priority information, I was able to redesign and reimplement the component in a less prominent but more practical way.`,
+      technicalPractices:
+        'IntersectionObserver API for LazyLoading & scroll to navigation, EmailJS API allowing direct messaging from browser to inbox, File-saver libary for direct downloading of CV from react, Carousel with UseSwipable hook to allow swiping between slides, Form validation with invalid form error handling, Various CSS layout/positioning examples, CSS failsafes to prevent sticky hover effects on mobile',
     },
     {
       title: 'Burger Builder',
@@ -26,12 +27,14 @@ const ProjectSlider = ({ showModal }) => {
       gitHubLink: 'https://github.com/Kwarn/burger-builder',
       courseLink:
         'https://www.udemy.com/course/react-the-complete-guide-incl-redux/',
-      description: `This app allows users to create and order their own customized burger.
+      description: `Built alongside React The Complete Guide, This app allows users to create and order their own customized burger.
 
-       Functionalities include form validation, authentication, database management, automatic login and preservation of the users session.`,
-      whatILearned: `This project was instrumental in developing an effective & efficient approach to learning new concepts. 
-      By using a compartmentalizing approach to the more difficult parts of the project, I would guide myself with the map of the user's experience and learn each required technique in digestible chunks. Often taking notes and using the rubber duck technique to reinforce my learnings`,
-      technicalPractices: `Multiple approaches to handling State, Authentication tokens and localStorage, Asynchronous actions, Class-based components and lifecycle hooks, Functional components and react hooks, Higher order components, Database rules configuration`,
+      The project was an invaluable tool which helped cement my understanding of the varying differences and nuances in approach when developing responsive react applications.`,
+
+      whatILearned: `The difference between class-based vs functional components, lifecycle hooks and React hooks. 
+
+      One of the final practices of this project was to convert the app from mostly older class-based components using lifecycle hooks (componentDidMount) to newer functional components using React Hooks (useEffect).  This played a vital role in reaffirming my overall understanding of React.`,
+      technicalPractices: `Multiple approaches to handling State, Authentication tokens and localStorage, Preservation of user's session, Promises & Asynchronous actions with Thunk/Sagas & Fetch/Axios, Class-based components and lifecycle hooks, Functional components and react hooks, Higher order components, Database rules configuration`,
     },
     {
       title: 'Square Playground',
@@ -39,31 +42,30 @@ const ProjectSlider = ({ showModal }) => {
       liveDemoLink: 'https://square-playground.web.app/',
       previewTechStack: 'Javascript, React, Redux, Css, Html',
 
-      gitHubLink: 'placeholder',
-      description: `A slot machine game based on simple Math.random() logic.
+      gitHubLink: 'https://github.com/Kwarn/square-playground',
+      description:`A slot machine game based on simple Math.random() logic. This was one of my first practice apps diving into the world of JavaScript and React. 
 
-       This was my first practice app diving into the world of javascript and React, the app was built mostly on guess work and google searches.`,
-      whatILearned: `This project taught me the importance of how to effectively find solutions and when it was acceptable to ask for help.
+      Built on a lot of guess work and trial and error the experience inspired my love for programming and the reward for overcoming challenges, it taught me to persevere when difficult unknown technical concepts seemed insurmountable.`,
+      whatILearned: `This project taught me how to effectively find solutions and when it was acceptable to ask for help.
 
       An early build of this project was experiencing performance issues caused by large image file sizes and complicated logic. While in pursuit of a solution to the complicated logic, I was introduced to Big O notation which helped me to understand why and how to refactor the code.`,
-      technicalPractices: 'Fundamentals of state and data management',
+      technicalPractices: 'Fundamentals of state and data management, Redux & Immer working with immutable state, Bootstrap with React',
     },
     {
       title: 'Fizzy-Lookup',
       image: imageAssets.fizzyLookup,
       previewTechStack: 'Python, tkinter',
-      gitHubLink: 'placeholder',
+      gitHubLink: 'https://github.com/Kwarn/Fizzy-Lookup',
       description: `Desktop App which allows easier searching of excel data.
 
-      Built for the head of lettings for a boutique serviced apartments company, this project allowed me demonstrate and practice a real world use-case of what I had learned with Python, helping them to work more efficiently on what was previously a time consuming and tedious task.
+      Built for the head of lettings for a boutique serviced apartments company, this project allowed me demonstrate and practice a real world use-case of what I had learned with Python.
       
-      The app uses pandas (data analysis library) to convert an excel spread-sheet to columns and rows displayed by tkinter (GUI library).
-      The excel spread-sheet contains property data such as rental price & number of bedrooms. The user can accurately search the data using a combination of 7 drop-down menus.`,
+      The app uses pandas (data analysis library) to convert an excel spread-sheet to columns and rows displayed by tkinter (GUI library). The user can accurately search the data using a combination of 7 drop-down menus.`,
       whatILearned: `The importance of accurately communicating a description 
         of the clients needs so that unnecessary or miscommunicated functionality
         didn't waste development time.`,
 
-      technicalPractices: 'System process access',
+      technicalPractices: 'Importing and reading XML files with Pandas, Tkinter scrollable canvas frames, Multiple uses of widgets, Grid place & pack layout management, Commutative search algorithm',
     },
     {
       isFirstElementOfTag: true,
@@ -72,12 +74,11 @@ const ProjectSlider = ({ showModal }) => {
       previewTechStack: 'Python, Tkinter, SQlite3',
 
       gitHubLink: 'https://github.com/Kwarn/fitness',
-      description: `A light-weight MyFitnessPal clone, this app allows users to save their meal history and add new meals with their ingredients and calorie information to a SQlite database.`,
-      whatILearned: `I learned why not to reinvent the wheel.
+      description: `A light-weight MyFitnessPal clone, this app allows users to save their meal history and add new meals with their ingredients and caloric information to a SQlite database.`,
+      whatILearned: `I learned the importance of looking for a cleaner, established solution before implementing my own.
 
-      My initial approach to storing data used the file system to read/write to a .txt file. I found this approach to be extremely cumbersome. In my search for a better solution I took the time to weight up the different options and settled on using SQLite database. 
-      I learned the importance of looking for a cleaner, established solution before implementing my own.`,
-      technicalPractices: 'Database queries',
+      My initial approach to storing data used the file system to read/write to a .txt file. I found this approach to be extremely cumbersome. In my search for a better solution I took the time to weight up the different options and settled on using SQLite database.`,
+      technicalPractices: 'Storing user input, Database management, Tkinter widgets,  Grid place & pack layout management',
     },
   ])
 
@@ -97,10 +98,7 @@ const ProjectSlider = ({ showModal }) => {
         className={classes.Slide}
         style={{ transform: `translateX(${x}%)` }}
       >
-        <Project
-          project={project}
-          showModal={showModal}
-        />
+        <Project project={project} showModal={showModal} />
       </div>
     )
   })
