b522eba50261a7fad79be522d013b3a3dd8cfecf
Kwarn <karlwarner.dev@gmail.com>
2020-10-01 16:21:00 +0100

    downloadIcon replaces text, css tweaks

--- src/components/Navigation/NavigationItems/NavigationItemsRight/NavigationItemsRight.js
+++ src/components/Navigation/NavigationItems/NavigationItemsRight/NavigationItemsRight.js
@@ -2,6 +2,7 @@ import React from 'react'
 import FileSaver from 'file-saver'
 import NavigationItem from '../NavigationItem/NavigationItem'
 import classes from './NavigationItemsRight.module.css'
+import imageAssets from '../../../../assets/assets'
 
 const saveFile = () =>
   FileSaver.saveAs(process.env.PUBLIC_URL + '/resource/test.jpg', 'test.jpg')
@@ -10,7 +11,8 @@ const navigationItemsRight = props => {
   return (
     <ul className={classes.NavigationItems}>
       <div className={classes.DownloadCV} onClick={saveFile}>
-        Download CV
+        <p>CV</p>
+        <img src={imageAssets.downloadIcon} alt="download CV"></img>
       </div>
       <NavigationItem link="/contact">Contact</NavigationItem>
     </ul>
