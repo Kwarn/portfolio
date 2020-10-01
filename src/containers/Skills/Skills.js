4caf20915ff9356dbe7a7c0282b13447c98c8347
Kwarn <karlwarner.dev@gmail.com>
2020-10-01 13:37:48 +0100

    removed redundant import

--- src/containers/Skills/Skills.js
+++ src/containers/Skills/Skills.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Skill from './Skill/Skill'
 import imageAssets from '../../assets/assets'
 import classes from './Skills.module.css'
