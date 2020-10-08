d0ef67315740ce1f375f5a861f9addef754c6557
Kwarn <karlwarner.dev@gmail.com>
2020-10-08 14:28:45 +0100

    projects descriptions reworded/updated

--- src/containers/ProjectSlider/ProjectSlider.js
+++ src/containers/ProjectSlider/ProjectSlider.js
@@ -11,11 +11,12 @@ const ProjectSlider = ({ showModal }) => {
       image: imageAssets.portfolio,
       previewTechStack: 'Javascript, React, Firebase, CSS',
       gitHubLink: 'https://github.com/Kwarn/portfolio',
-      description: `Serving as a live environment to create and tinker with components, the portfolio has been an invaluable resource allowing me to practise a variety of design approaches, receive feedback and consider different perspectives.`,
-      whatILearned: `I learned the importance of remaining objective to prevent the over implementation of a feature having detrimental effects to user’s experience and their time constraints.
-
-      Early on in the project I used the creation of the ‘Draw/More Info’ component (bottom right) to challenge my theoretical implementation before resorting to google. Success served to reinforce my technical understanding and came with a boost of confidence.
-      However, I realised with the over implimented approach I was taking; the user experience would suffer when being forced to click most elements to view high priority information, I was able to redesign and reimplement the component in a less prominent but more practical way.`,
+      description: `Serving as a live environment to create and tinker with components, the portfolio has been an invaluable resource allowing me to practise a variety of design approaches, receive feedback and consider different perspectives.
+      
+      Throughout the project I’ve challenged myself to implement components & features from the ground up, with the goal of achieving a strong grasp of how popular components work.`,
+      whatILearned: `How to debug effectively, finding and adapting solutions to my specific case. 
+      With many of these challenges being untrodden ground, learning how to debug efficiently has been crucial to maintaining positivity, progress and keeping to my deadlines.
+      My understanding and accuracy when predicting where and why bugs are likely to occur has dramatically improved through practice. I’ve learned multiple techniques for locating the source of issues and thrive on the process of weeding out bad solutions and honing in on the best solutions for my case.`,
       technicalPractices:
         'IntersectionObserver API for LazyLoading & scroll to navigation, EmailJS API allowing direct messaging from browser to inbox, File-saver libary for direct downloading of CV from react, Carousel with UseSwipable hook to allow swiping between slides, Form validation with invalid form error message handling, Various CSS layout/positioning examples, CSS failsafes to prevent sticky hover effects on mobile, Fully responsive design catering from 4k users to 320x568 iPhone 5 users',
     },
@@ -29,12 +30,12 @@ const ProjectSlider = ({ showModal }) => {
         'https://www.udemy.com/course/react-the-complete-guide-incl-redux/',
       description: `Built alongside React The Complete Guide, This app allows users to create and order their own customized burger.
 
-      The project was an invaluable tool which helped cement my understanding of the differences and nuances in approach when developing responsive react applications.`,
+      The project greatly helped cement my understanding of React, Redux and the differences and nuances in approach when developing responsive react applications.`,
 
-      whatILearned: `The difference between class-based vs functional components, lifecycle hooks and React hooks. 
+      whatILearned: `The difference between new and old approaches.
 
-      One of the final practices of this project was to convert the app from mostly older class-based components using lifecycle hooks (componentDidMount) to newer functional components using React Hooks (useEffect).  This played a vital role in reaffirming my overall understanding of React.`,
-      technicalPractices: `Multiple approaches to handling State, Authentication tokens and localStorage, Preservation of user's session, Promises & Asynchronous actions with Thunk/Sagas & Fetch/Axios, Class-based components and lifecycle hooks, Functional components and react hooks, Higher order components, React.Memo and performance tweaks, Database rules configuration`,
+      My final practice in this project was to convert the app from mostly older class-based components using lifecycle hooks (componentDidMount) to newer functional components using React Hooks (useEffect).  This played a vital role in reaffirming my overall understanding of React.`,
+      technicalPractices: `Multiple approaches to handling State, Preservation of user's session with Authentication tokens and localStorage, Promises & Asynchronous actions with Thunk/Sagas & Fetch/Axios, Class-based components and Functional components, Higher order components, React.Memo and performance tweaks, Database rules configuration`,
     },
     {
       title: 'Square Playground',
@@ -43,13 +44,14 @@ const ProjectSlider = ({ showModal }) => {
       previewTechStack: 'Javascript, React, Redux, Css, Html',
 
       gitHubLink: 'https://github.com/Kwarn/square-playground',
-      description:`A slot machine game based on simple Math.random() logic. This was one of my first practice apps diving into the world of JavaScript and React. 
+      description: `A slot machine game based on Math.random() logic. This was one of my first practice apps diving into the world of JavaScript and React. 
 
       Built on a lot of guess work and trial and error the experience inspired my love for programming and the reward for overcoming challenges, it taught me to persevere when difficult unknown technical concepts seemed insurmountable.`,
-      whatILearned: `This project taught me how to effectively find solutions and when it was acceptable to ask for help.
+      whatILearned: `This project taught me patience and an eye for detail. Being one of my first javascript apps, I found myself often chasing solutions to problems stemming from what I would now consider a trivial or silly source. The experience provided me a better gauge as to when it was acceptable to ask for help.
 
       An early build of this project was experiencing performance issues caused by large image file sizes and complicated logic. While in pursuit of a solution to the complicated logic, I was introduced to Big O notation which helped me to understand why and how to refactor the code.`,
-      technicalPractices: 'Fundamentals of state and data management, Redux & Immer working with immutable state, Bootstrap with React',
+      technicalPractices:
+        'Fundamentals of state and data management, Redux & Immer working with immutable state, Bootstrap with React',
     },
     {
       title: 'Fizzy-Lookup',
@@ -65,7 +67,8 @@ const ProjectSlider = ({ showModal }) => {
         of the clients needs so that unnecessary or miscommunicated functionality
         didn't waste development time.`,
 
-      technicalPractices: 'Importing and reading XML files with Pandas, Tkinter scrollable canvas frames, Multiple uses of widgets, Grid place & pack layout management, Commutative search algorithm',
+      technicalPractices:
+        'Importing and reading XML files with Pandas, Tkinter scrollable canvas frames, Multiple uses of widgets, Grid place & pack layout management, Commutative search algorithm',
     },
     {
       title: 'Fitness',
@@ -73,11 +76,12 @@ const ProjectSlider = ({ showModal }) => {
       previewTechStack: 'Python, Tkinter, SQlite3',
 
       gitHubLink: 'https://github.com/Kwarn/fitness',
-      description: `A light-weight MyFitnessPal clone, this app allows users to save their meal history and add new meals with their ingredients and caloric information to a SQlite database.`,
+      description: `A light-weight MyFitnessPal clone, this was my first python project. This app allows users to save their meal history and add new meals with their ingredients and caloric information to a SQlite database.`,
       whatILearned: `I learned the importance of looking for a cleaner, established solution before implementing my own.
 
       My initial approach to storing data used the file system to read/write to a text file. I found this approach to be extremely cumbersome. In my search for a better solution I took the time to weight up the different options and settled on using SQLite database.`,
-      technicalPractices: 'Storing user input, Database management, Tkinter widgets,  Grid place & pack layout management',
+      technicalPractices:
+        'Storing user input, Database management, Tkinter widgets,  Grid place & pack layout management',
     },
   ])
 
