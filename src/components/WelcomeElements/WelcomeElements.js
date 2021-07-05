067c61a5338f9158025ad68d2e7eb4d4818b2ab0
Kwarn <karlwarner.dev@gmail.com>
2021-07-05 11:56:34 +0100

    Links are no longer part of WelcomeElements but a menuItem

--- src/components/WelcomeElements/WelcomeElements.js
+++ src/components/WelcomeElements/WelcomeElements.js
@@ -16,25 +16,6 @@ const WelcomeElements = () => {
           <div>
             <h1>Karl Warner</h1>
             <p>{`</> Full-Stack Web Developer`}</p>
-            <div className={classes.LinkIcons}>
-              <a
-                href="https://github.com/Kwarn/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={imageAssets.gitHub} alt="gitHubIcon"></img>
-              </a>
-              <a
-                href="https://www.linkedin.com/in/karl-warner-9147661b5/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={imageAssets.linkedInCharcoal}
-                  alt="linkedInIcon"
-                ></img>
-              </a>
-            </div>
           </div>
         </div>
       </div>
