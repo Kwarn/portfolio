1a4e9bea9cb8d4b94090685549d17f518c721e1a
Kwarn <karlwarner.dev@gmail.com>
2021-07-05 23:00:05 +0100

    Welcome Elements moved to Dashboard folder

---
+++ src/containers/Dashboard/WelcomeElements/WelcomeElements.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import imageAssets from '../../../assets/assets';
+import classes from './WelcomeElements.module.css';
+
+const WelcomeElements = () => {
+  return (
+    <div className={classes.WelcomeElementsWrapper}>
+      <div className={classes.WelcomeElements}>
+        <div className={classes.Background} />
+        <img
+          className={classes.ProfileImage}
+          src={imageAssets.profileImage}
+          alt="profileImage"
+        />
+        <div className={classes.TitleGroup}>
+          <div>
+            <h1>Karl Warner</h1>
+            <p>{`</> Full-Stack Web Developer`}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomeElements;
