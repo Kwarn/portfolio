ba6ee9108eaf6fae265af4af0e981f613a46d617
Kwarn <karlwarner.dev@gmail.com>
2021-07-05 22:53:59 +0100

    Styling tweaks & HELLO added

--- src/containers/Dashboard/AboutMe/AboutMe.jsx
+++ src/containers/Dashboard/AboutMe/AboutMe.jsx
@@ -7,27 +7,40 @@ const StyledAboutMeContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
+  color: #c5c6c7;
+`;
+
+const StyledQuote = styled.p`
+  margin: 15vh auto auto auto;
+  width: 50%;
 `;
 
 const StyledText = styled.p`
   margin: auto;
   width: 50%;
-  color: #c5c6c7;
+`;
+
+const StyledHeader = styled.h1`
+  font-size: 15em;
+  margin-left: 5vw;
 `;
 
 export default function AboutMe() {
   return (
     <StyledAboutMeContainer>
-      <StyledText>
-        “A good programmer is someone who always looks both ways before crossing
-        a one-way street.”
-      </StyledText>
+      <StyledQuote>
+        {`“A good programmer is someone who always looks
+         both ways before crossing a one-way street.” - Some bloke`}
+      </StyledQuote>
+      <StyledHeader>HELLO</StyledHeader>
       <StyledText>
         A devout learner experienced in Full-stack development with a focus on
         front-end systems, utilizing popular modern frameworks & libraries
         whilst practising the latest generation of architecture and design
-        principles to create responsive, scaleable, andinitiative web solutions.
+        principles to create responsive, scaleable, and initiative web
+        solutions.
       </StyledText>
+      <StyledText></StyledText>
     </StyledAboutMeContainer>
   );
 }
