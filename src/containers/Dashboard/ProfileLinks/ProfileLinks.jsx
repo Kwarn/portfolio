68677a6f6cd12d66d1b303b6fef0f58b2bf71e1b
Kwarn <karlwarner.dev@gmail.com>
2021-07-10 18:33:57 +0100

    ProfileLinks converted to styled-components from CSS modules.

--- src/containers/Dashboard/ProfileLinks/ProfileLinks.jsx
+++ src/containers/Dashboard/ProfileLinks/ProfileLinks.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
 import imageAssets from '../../../assets/assets';
-import classes from './ProfileLinks.module.css';
 import FileSaver from 'file-saver';
+import styled, { keyframes } from 'styled-components';
+
+const animation = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-5px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
+const StyledWrapper = styled.div`
+  width: 100%;
+  margin: 0 auto 10px auto;
+  display: flex;
+  justify-content: space-evenly;
+  border-bottom: 4px solid #c5c6c7;
+`;
+
+const StyledLink = styled.a`
+  z-index: 101;
+  cursor: pointer;
+  &:hover {
+    animation: ${animation} 1s;
+  }
+`;
+
+const StyledImage = styled.img`
+  width: auto;
+  /* height: ${props => (props.small ? '30px' : '40px')} */
+  height: 40px;
+  /* margin: ${props => (props.small ? 'auto auto 5px auto' : 'auto')}; */
+  cursor: pointer;
+  &:hover {
+    animation: ${animation} 1s;
+  }
+`;
 
 export default function ProfileLinks() {
   const saveFile = () =>
@@ -10,24 +49,27 @@ export default function ProfileLinks() {
       'Karl_Warner_CV.pdf'
     );
   return (
-    <div className={classes.ProfileLinks}>
-      <a
+    <StyledWrapper>
+      <StyledLink
         href="https://github.com/Kwarn/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <img src={imageAssets.gitHubLight} alt="gitHubIcon"></img>
-      </a>
-      <a
+        <StyledImage src={imageAssets.gitHubLight} alt="gitHubIcon" />
+      </StyledLink>
+      <StyledLink
         href="https://www.linkedin.com/in/karl-warner-9147661b5/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <img src={imageAssets.linkedInLight} alt="linkedInIcon"></img>
-      </a>
-      <div className={classes.DownloadCV} onClick={saveFile}>
-        <img src={imageAssets.cvIcon} alt="download CV" />
-      </div>
-    </div>
+        <StyledImage src={imageAssets.linkedInLight} alt="linkedInIcon" />
+      </StyledLink>
+      <StyledImage
+        onClick={saveFile}
+        small={true}
+        src={imageAssets.cvIcon}
+        alt="download CV"
+      />
+    </StyledWrapper>
   );
 }
