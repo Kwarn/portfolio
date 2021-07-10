0ecf08bf1742e3805cace5b4ca492ade3cef6ccb
Kwarn <karlwarner.dev@gmail.com>
2021-07-10 18:38:35 +0100

    Home ->  removed redundant onResize/layout logic -> layout is now controlled by HOC Layout.

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,51 +5,17 @@ import Dashboard from '../Dashboard/Dashboard';
 import WebFont from 'webfontloader';
 
 const Home = () => {
-  const [modalControl, setModalControl] = useState({
-    isOpen: false,
-    modalContent: null,
-  });
-
-  const [windowDimensions, setWindowDimensions] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
   useEffect(() => {
-    window.addEventListener('resize', updateWindowDimensions);
     WebFont.load({
       google: {
         families: ['Teko'],
       },
     });
-    return () => {
-      window.removeEventListener('resize', updateWindowDimensions);
-    };
   }, []);
-
-  const updateWindowDimensions = () =>
-    setWindowDimensions({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    });
-
-  const showModalHandler = modalContent => {
-    setModalControl({ modalContent: modalContent, isOpen: true });
-  };
-
-  const hideModalHandler = () => {
-    setModalControl({ ...modalControl.modalContent, isOpen: false });
-  };
-
-  const modal = (
-    <Modal show={modalControl.isOpen} hide={() => hideModalHandler()}>
-      {modalControl.modalContent}
-    </Modal>
-  );
 
   return (
     <div className={classes.DashboardWrapper}>
-      <Dashboard showModal={modalContent => showModalHandler(modalContent)} />
+      <Dashboard />
     </div>
   );
 };
