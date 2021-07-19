b776d04dff4da3877bf6d87419ebd8c16fb0a8b3
Kwarn <karlwarner.dev@gmail.com>
2021-07-19 13:40:20 +0100

    CodeChallenges: background added, css tweaks.

--- src/containers/Dashboard/CodeChallenges/ExtraInfo.js
+++ src/containers/Dashboard/CodeChallenges/ExtraInfo.js
@@ -31,45 +31,47 @@ const ExtraInfo = ({ showModal }) => {
   I'm constantly challenging myself by not just solving the problem but also searching for and understanding the most elegant solutions. So far, I've completed all challenges on CodingBat, earned 1800 XP on Edabit solving primarily medium-hard difficulty problems and am currently working through certification on HackerRank.`;
 
   return (
-    <div className={classes.ExtraInfo}>
-      <h2>Code Challenges</h2>
-      <p>{description}</p>
-      <hr />
-      <h2>Profiles & Resources</h2>
-      {imageWithLink(
-        imageAssets.hackerRank,
-        'hackerRank',
-        'https://www.hackerrank.com/karlwarner_dev'
-      )}
-      {imageWithLink(
-        imageAssets.edabit,
-        'edabit',
-        'https://edabit.com/user/qgYikiD7WB8JciTdo'
-      )}
-      {imageWithLink(
-        imageAssets.codingBat,
-        'codingbat',
-        'https://codingbat.com/done?user=karlwarner.dev@gmail.com&tag=6083086670'
-      )}
-      {imageWithLink(
-        imageAssets.udemy,
-        'udemy',
-        'https://www.udemy.com/user/karl-warner-5/'
-      )}
-      {imageWithLink(
-        imageAssets.freeCodeCamp,
-        'freeCodeCamp',
-        'https://www.freecodecamp.org/karl_warner'
-      )}
-      {imageWithLink(
-        imageAssets.codeCademy,
-        'codecademy',
-        'https://www.codecademy.com/profiles/Xeptic'
-      )}
-      <hr />
-      <div className={classes.Resources}></div>
-      {imageWithModal(imageAssets.theArtOfReadableCode)}
-      {imageWithModal(imageAssets.becomingABetterProgrammer)}
+    <div className={classes.Wrapper}>
+      <div className={classes.ExtraInfo}>
+        <h2>Code Challenges</h2>
+        <p>{description}</p>
+        <hr />
+        <h2>Profiles & Resources</h2>
+        {imageWithLink(
+          imageAssets.hackerRank,
+          'hackerRank',
+          'https://www.hackerrank.com/karlwarner_dev'
+        )}
+        {imageWithLink(
+          imageAssets.edabit,
+          'edabit',
+          'https://edabit.com/user/qgYikiD7WB8JciTdo'
+        )}
+        {imageWithLink(
+          imageAssets.codingBat,
+          'codingbat',
+          'https://codingbat.com/done?user=karlwarner.dev@gmail.com&tag=6083086670'
+        )}
+        {imageWithLink(
+          imageAssets.udemy,
+          'udemy',
+          'https://www.udemy.com/user/karl-warner-5/'
+        )}
+        {imageWithLink(
+          imageAssets.freeCodeCamp,
+          'freeCodeCamp',
+          'https://www.freecodecamp.org/karl_warner'
+        )}
+        {imageWithLink(
+          imageAssets.codeCademy,
+          'codecademy',
+          'https://www.codecademy.com/profiles/Xeptic'
+        )}
+        <hr />
+        <div className={classes.Resources}></div>
+        {imageWithModal(imageAssets.theArtOfReadableCode)}
+        {imageWithModal(imageAssets.becomingABetterProgrammer)}
+      </div>
     </div>
   );
 };
